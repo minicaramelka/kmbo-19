8d277a72b595d131b5025dbff0a1e4b3ec1c73e0
minicaramelka <59147993+minicaramelka@users.noreply.github.com>
2020-04-24 19:03:27 +0300

    Update llist.h

--- llist.h
+++ llist.h
@@ -1,29 +1,43 @@
 #ifndef LLIST_H
 #define LLIST_H
-
 #include <cstddef>
 
 // Linked list of integers
 
-class LList
-{
-	// HERE WILL BE YOUR CODE
+class LList{
+
+    struct Node{
+        int data;
+        Node *next;
+    };
+
+	private:
+   	Node *head;
 
 	public:
-
 	LList();                         // construct new collection
+	LList(const LList& copyLLis);
+	LList(LList&& copyList){
+		this->head = copyList.head;
+		copyList.head = nullptr;
+	}
 	~LList();                        // free resources
+	void forceNodeDelete(Node* node);
+
 
 	void push_back(int val);         // add new value at the end:  [1 2 3 4] -> [1 2 3 4 5]
 	void push_front(int val);        // add new value at the begin [1 2 3 4] -> [5 1 2 3 4]
-	void pop_back(int val);          // remove at the end          [1 2 3 4] -> [1 2 3]
-	void pop_front(int val);         // remove at the front        [1 2 3 4] -> [2 3 4]
+	void pop_back();          // remove at the end          [1 2 3 4] -> [1 2 3]
+	void pop_front();         // remove at the front        [1 2 3 4] -> [2 3 4]
 	size_t size() const;             // get actual number of items [1 2 3 4] -> 4
 	int& operator[](size_t idx);     // get rw access ot specific item addressing by idx
-	int  operator[](size_t idx) const; //get read-only access
+	int  operator[](size_t idx)const; //get read-only access
 	void erase_at(size_t idx);       // remove item at specific position: [1 2 3 4], 2 -> [1 2 4]
 	void insert_at(size_t idx, int val); // insert item at specific position: [1 2 3 4], 1, 5 -> [1 5 2 3 4]
-	void reverse();                  // reverse item sequense: [1 2 3 4] -> [4 3 2 1]
+	void reverse();    // reverse item sequense: [1 2 3 4] -> [4 3 2 1]
 };
+
+#endif //LLIST_H
+
 
 #endif //LLIST_H
