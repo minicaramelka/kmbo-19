9ac5e89cb96f2e06283357e346500f0ce4e5e9f9
minicaramelka <59147993+minicaramelka@users.noreply.github.com>
2020-04-24 19:04:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
-#include <llist.h>
-
+#include "llist.h"
+#include "list.cpp"
 using namespace std;
-
-void dump(const LList& l)
-{
+   
+void dump(const LList& l){
 	for(size_t i = 0; i < l.size(); ++i, cout << " ") cout << l[i];
-}
+}   
 
-int main()
-{
-	LList l;
-	l.push_back(1);
-	l.push_front(0);
-	l.push_back(2);
+int main(){
+	//with move
 
-	cout << "Stage 1: "; dump(l); cout << endl;
+	//LList l;
+	//for(int i = 0;i < 3000;i++)
+	//	l.push_back(i);
+	//LList a(move(l));
 
-	l.insert_at(1, 10);
-	cout << "Stage 2: "; dump(l); cout << endl;
+	//with copy
 
-	l.erase_at(1);
-	cout << "Stage 2: "; dump(l); cout << endl;
-
-	l.reverse();
-	cout << "Stage 3: "; dump(l); cout << endl;
+        //LList m;
+        //for(int i = 0;i < 3000;i++)
+        //	m.push_back(i);
+        //LList b(m);
 
 	return 0;
 }
